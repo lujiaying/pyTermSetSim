e3965d7f291b84674e62f75dbe74574a0c9974ca
Wenjing Ma <wma2026@cuhk.edu.cn>
2026-05-26 15:24:07 +0800

    first version

--- pytermsetsim/evaluate.py
+++ pytermsetsim/evaluate.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 from scipy.optimize import linear_sum_assignment
 import numpy as np
@@ -7,10 +7,28 @@ from sentence_transformers import SentenceTransformer
 
 def term_set_sim_by_sim_matrix(
     sim_matrix: np.ndarray
-):
+) -> Tuple[float, list, list]:
+    """
+    sim_matrix: row indicates gold terms, column indicates predicted terms. Each entry is the similarity between the corresponding gold and predicted term.
+    """
     cost_matrix = 1 - sim_matrix  # Convert similarity to cost
+    # Hungarian algorithm to find the optimal assignment; ECCV's DETR paper also uses similar idea
     row_ind, col_ind = linear_sum_assignment(cost_matrix)
-    return row_ind, col_ind
+    sim_score_sum = sim_matrix[row_ind, col_ind].sum()
+    sim_score_avg = sim_score_sum / len(row_ind)
+    return sim_score_avg, row_ind, col_ind
+
+
+def term_set_sim_by_sentence_transformer(
+    gold: List[str],
+    pred: List[str],
+    model: SentenceTransformer
+) -> Tuple[float, list, list, np.ndarray]:
+    gold_embs = model.encode(gold)
+    pred_embs = model.encode(pred)
+    sim_matrix = model.similarity(gold_embs, pred_embs)
+    sim_score,row_ind, col_ind = term_set_sim_by_sim_matrix(sim_matrix)
+    return sim_score, row_ind, col_ind, sim_matrix
 
 
 if __name__ == "__main__":
@@ -22,16 +40,17 @@ if __name__ == "__main__":
         "pyrexia"
     ]
     pred = [
-        "hypertension",
         "dry cough",
-        "fever"
+        "fever",
+        "hypertension",
     ]
     gold_embs = model.encode(gold)
     pred_embs = model.encode(pred)
     sim_matrix = model.similarity(gold_embs, pred_embs)
     print(f"Similarity matrix:", sim_matrix)
-    row_ind, col_ind = term_set_sim_by_sim_matrix(sim_matrix)
+    sim_score, row_ind, col_ind = term_set_sim_by_sim_matrix(sim_matrix)
     print("Optimal assignment:", list(zip(row_ind, col_ind)))
+    print(f'Similarity score: {sim_score}')
 
     # case 2
     gold = [
@@ -40,6 +59,10 @@ if __name__ == "__main__":
         "pyrexia"
     ]
     pred = [
+        "fever",
         "hypertension",
-        "fever"
     ]
+    sim_score, row_ind, col_ind, sim_matrix = term_set_sim_by_sentence_transformer(gold, pred, model)
+    print(f"Similarity matrix:", sim_matrix)
+    print("Optimal assignment:", list(zip(row_ind, col_ind)))
+    print(f'Similarity score: {sim_score}')
