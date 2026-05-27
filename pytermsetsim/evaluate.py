1095411715c53648096f1809c1f44834397c2475
Wenjing Ma <wma2026@cuhk.edu.cn>
2026-05-27 12:11:24 +0800

    upgrade to prec, recall, f1

--- pytermsetsim/evaluate.py
+++ pytermsetsim/evaluate.py
@@ -5,7 +5,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 
-def term_set_sim_by_sim_matrix(
+def _helper_SBMS(
     sim_matrix: np.ndarray
 ) -> Tuple[float, list, list]:
     """
@@ -15,20 +15,25 @@ def term_set_sim_by_sim_matrix(
     # Hungarian algorithm to find the optimal assignment; ECCV's DETR paper also uses similar idea
     row_ind, col_ind = linear_sum_assignment(cost_matrix)
     sim_score_sum = sim_matrix[row_ind, col_ind].sum()
-    sim_score_avg = sim_score_sum / len(row_ind)
-    return sim_score_avg, row_ind, col_ind
+    return sim_score_sum, row_ind, col_ind
 
 
-def term_set_sim_by_sentence_transformer(
+def SBMS_by_sentence_transformer(
     gold: List[str],
     pred: List[str],
     model: SentenceTransformer
 ) -> Tuple[float, list, list, np.ndarray]:
+    """
+    SBMS stands for Soft Bipartite Matching Score.
+    """
     gold_embs = model.encode(gold)
     pred_embs = model.encode(pred)
     sim_matrix = model.similarity(gold_embs, pred_embs)
-    sim_score,row_ind, col_ind = term_set_sim_by_sim_matrix(sim_matrix)
-    return sim_score, row_ind, col_ind, sim_matrix
+    sim_score_sum, row_ind, col_ind = _helper_SBMS(sim_matrix)
+    sim_score_prec = sim_score_sum / len(pred) if len(pred) > 0 else 0.0
+    sim_score_rec = sim_score_sum / len(gold) if len(gold) > 0 else 0.0
+    sim_score_f1 = 2 * sim_score_prec * sim_score_rec / (sim_score_prec + sim_score_rec) if (sim_score_prec + sim_score_rec) > 0 else 0.0
+    return sim_score_f1, sim_score_prec, sim_score_rec, row_ind, col_ind, sim_matrix
 
 
 if __name__ == "__main__":
@@ -44,13 +49,10 @@ if __name__ == "__main__":
         "fever",
         "hypertension",
     ]
-    gold_embs = model.encode(gold)
-    pred_embs = model.encode(pred)
-    sim_matrix = model.similarity(gold_embs, pred_embs)
+    SBMS_f1, SBMS_prec, SBMS_rec, row_ind, col_ind, sim_matrix = SBMS_by_sentence_transformer(gold, pred, model)
     print(f"Similarity matrix:", sim_matrix)
-    sim_score, row_ind, col_ind = term_set_sim_by_sim_matrix(sim_matrix)
     print("Optimal assignment:", list(zip(row_ind, col_ind)))
-    print(f'Similarity score: {sim_score}')
+    print(f'Similarity f1: {SBMS_f1}, Precision: {SBMS_prec}, Recall: {SBMS_rec}')
 
     # case 2
     gold = [
@@ -62,7 +64,7 @@ if __name__ == "__main__":
         "fever",
         "hypertension",
     ]
-    sim_score, row_ind, col_ind, sim_matrix = term_set_sim_by_sentence_transformer(gold, pred, model)
+    SBMS_f1, SBMS_prec, SBMS_rec, row_ind, col_ind, sim_matrix = SBMS_by_sentence_transformer(gold, pred, model)
     print(f"Similarity matrix:", sim_matrix)
     print("Optimal assignment:", list(zip(row_ind, col_ind)))
-    print(f'Similarity score: {sim_score}')
+    print(f'Similarity f1: {SBMS_f1}, Precision: {SBMS_prec}, Recall: {SBMS_rec}')
